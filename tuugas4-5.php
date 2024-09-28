8d157571a17541b4ca86883b2bf35d880a22f1dd
Chandraa08 <162288666+Chandraa08@users.noreply.github.com>
2024-09-28 11:51:24 +0800

    Update tuugas4-5.php

--- tuugas4-5.php
+++ tuugas4-5.php
@@ -16,17 +16,14 @@ echo "<br><br>";
 echo "<soal no 5 <br><br>";
 
 
-// Input dari pengguna
 $usia = (int)readline("Masukkan usia pemohon: ");
 $penghasilan = (int)readline("Masukkan penghasilan bulanan pemohon (dalam Rp): ");
 $riwayatKredit = strtolower(readline("Masukkan riwayat kredit pemohon (macet/tidak macet): "));
 
-// Memeriksa kelayakan
 if ($usia >= 21 && $usia <= 60 && $penghasilan >= 5000000 && $riwayatKredit !== "macet") {
     echo "Pemohon Kredit Layak." . PHP_EOL;
 } else {
     echo "Pemohon Kredit Tidak Layak." . PHP_EOL;
 }
-
-
+    
 ?>
